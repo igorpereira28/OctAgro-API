c4869d679b2eb5f087a369500014b1bdea4bd10a
igorpereira28 <igorsilvape28@gmail.com>
2023-04-02 17:05:18 -0300

    Adiconando 2 novas pastas para gerenciamento do projeto

---
+++ config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    host: "localhost",
+    dialect: "mysql",
+    username: "root",
+    password: "123456",
+    database: "OctAgro-API",
+    define: {
+        timestamp: true,
+        undercored: true
+    }
+}
